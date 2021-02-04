da5ea004d987e1e2d462176c7103f0d8e1ceebcf
Adam Nagy <adam_e_nagy@hotmail.com>
2021-02-04 19:18:21 +0000

    Update MyExtension.ts

--- src/MyExtension.ts
+++ src/MyExtension.ts
@@ -1,8 +1,6 @@
 export class MyExtension extends Autodesk.Viewing.Extension {
   load() {
       console.log('MyExtension has been loaded');
-      this.viewer.setEnvMapBackground(false); // Hide background environment if there is one
-      this.viewer.setBackgroundColor(0, 64, 128, 0, 64, 128); // Set background color
       return true;
   }
 
